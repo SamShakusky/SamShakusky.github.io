ef22af5248bdfcabb8ca61c51f96f1b7d98a201b
SamShakusky <shakuskys@gmail.com>
2016-01-11 14:51:12 +0300

    optimization again

--- works/portfolio/js/isImgLoaded.js
+++ works/portfolio/js/isImgLoaded.js
@@ -1,7 +1,7 @@
 var isImgLoaded = function () {
     "use strict";
     
-    $('.container:nth-child(11)').imagesLoaded()
+    $('.container:nth-child(11), aside').imagesLoaded()
         .done(function () {
         $('.loading, .loading img, body').addClass('done');
   });

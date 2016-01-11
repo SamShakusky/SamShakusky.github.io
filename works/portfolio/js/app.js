a294c962b5cc2795711b69e3ab6fc64e3c17f951
SamShakusky <shakuskys@gmail.com>
2016-01-11 14:48:13 +0300

    cleaning and optimizing

--- works/portfolio/js/app.js
+++ works/portfolio/js/app.js
@@ -112,63 +112,9 @@ var swapPhotos = function () {
     });
 };
 
-/*var filterTags = function () {
-    "use strict";
-    
-    var revealAllContainers = function () {
-        $('.container').each(function () {
-            $(this).css('display', 'block');
-            $(this).children().removeClass('hidden');
-            $('.grid').css({'flex-direction': 'column', 'flex-wrap': 'wrap'});
-        });
-    };
-    
-    
-    $('.container').each(function () {
-            $(this).addClass('all');
-        });
-    
-    $('footer button').click(function () {
-        var theClass = $(this).attr('class');
-        
-        $('button#activeButton').attr('id', '');
-        $(this).attr('id', 'activeButton');
-        
-        if ($(this).hasClass('all')) {
-            revealAllContainers();
-            photoId = 1;
-        } else {
-            
-            revealAllContainers();
-            
-            $('.container').each(function () {
-                
-                
-                $(this).removeClass('container');
-                $(this).removeClass('all');
-
-
-                if ($(this).attr('class') !== theClass) {
-                    $(this).css('display', 'none');
-                    
-                    $('.grid').css({'flex-direction': 'row', 'flex-wrap': 'nowrap'});
-                    javascript:window.scrollTo(0, 0);
-                }
-                
-                $(this).addClass('container');
-                $(this).addClass('all');
-            });
-        }
-    });
-};*/    // unfinished
-
-
-
-
 
 $(document).ready(clickMenu);
 $(document).ready(clickContacts);
 $(document).ready(hoverEffects);
 $(document).ready(toggleGallery);
 $(document).ready(swapPhotos);
-/*$(document).ready(filterTags);*/
--- works/portfolio/js/isImgLoaded.js
+++ works/portfolio/js/isImgLoaded.js
@@ -1,7 +1,7 @@
 var isImgLoaded = function () {
     "use strict";
     
-    $('.container:nth-child(n+1):nth-child(-n+3), .container:nth-child(6):nth-child(7), .container:nth-child(n+1):nth-child(-n+3), .container:nth-child(n+11):nth-child(-n+13), .container:nth-child(n+17):nth-child(-n+19)').imagesLoaded()
+    $('.container:nth-child(11)').imagesLoaded()
         .done(function () {
         $('.loading, .loading img, body').addClass('done');
   });

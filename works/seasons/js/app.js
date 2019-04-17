ce9d04540b02e8c5c216315a59d203e52110103a
Eugene <shakuskys@gmail.com>
2019-04-18 00:35:14 +0300

    old portfolio cleanup

--- works/seasons/js/app.js
+++ works/seasons/js/app.js
@@ -1,40 +1,13 @@
-var date = new Date();
+"use strict";
 
-var season = function() {
-	"use strict";
-	
-	switch (date.getMonth()) {
-		case 11:
-		case 0:
-		case 1:
-			$('body').css('background', 'url(img/winter.jpg)');
-			$('h1').html('Сейчас Зима');
-			break
-		case 2:
-		case 3:
-		case 4:
-			$('body').css('background', 'url(img/spring.jpg)');
-			$('h1').html('Сейчас Весна');
-			break
-		case 5:
-		case 6:
-		case 7:
-			$('body').css('background', 'url(img/summer.jpg)');
-			$('h1').html('Сейчас Лето');
-			break
-		case 8:
-		case 9:
-		case 10:
-			$('body').css('background', 'url(img/autmn.jpg)');
-			$('h1').html('Сейчас Осень');
-			break
-		default:
-			alert('Ooops');
-	}
-	
-	$('body').css('background-size', 'cover');
-	$('body').css('background-position', 'bottom');
-	
-};
+var seasons = ['Зима', 'Весна', 'Лето', 'Осень'];
+var currentMonth = new Date().getMonth();
+var currentSeason = Math.floor((currentMonth / 12 * 4)) % 4;
+var text = 'Сейчас '+ seasons[currentSeason];
 
-$(document).ready(season);
+var heading = document.getElementById('heading');
+var body = document.getElementById('body');
+
+document.title = text;
+heading.innerText = text;
+body.classList = 'season-' + currentSeason;
